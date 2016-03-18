81b2de18d61f25ae6a7169735d10c76daf628712
Martin <martinarroyo@usal.es>
2016-03-18 19:42:41 +0100

    File downloading using AJAX

--- backend/service.py
+++ backend/service.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
 import tornado.ioloop
 import tornado.web
 import json
 import os, sys
+import tempfile
+from zipfile import ZipFile
+
+from io import StringIO ## TODO: Have a look at cStringIO
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 STATIC_DIR = os.path.join(CURRENT_DIR, 'static')
+
 
 PORT = 9000
 """
@@ -23,17 +29,73 @@ class IndexHandler(tornado.web.RequestHandler):
 """
 
 class RenderHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        zipfile_name = "/Users/martin/Desktop/demo.zip"
+        with open(zipfile_name, 'rb') as z:
+            self.set_header('Content-Type', 'application/zip')
+            self.write(z.read())
+            self.finish()
+
+
     def post(self):
-        markdown_data = self.get_body_argument('text-input-area', '')
-        self.write("Good")
+        markdown_data = self.get_body_argument('markdown-data', '')
+
+        zipfile_name = "/Users/martin/Desktop/demo.zip"
+        zip_file = ZipFile(zipfile_name, mode='w')
+        zip_file.writestr('dir/markdown.tex', markdown_data)
+        for key in self.request.files.keys():
+            zip_file.writestr('dir/images/%s' % key, self.request.files[key][0]['body'])
+
+        zip_file.close()
+        with open(zipfile_name, 'rb') as z:
+            self.set_header('Content-Type', 'application/zip')
+            #self.set_header("Content-Disposition", "attachment; filename=\"{$strFileName}\"");
+
+            #self.set_header("Content-Transfer-Encoding", "binary");
+            #z.seek(0, os.SEEK_END)
+            #self.set_header("Content-Length", z.tell());
+
+            self.write(z.read())
+
+            self.finish()
+        #self.write("OK")
+
+        # markdown_data = self.get_body_argument('markdown-data', '')
+        #
+        # with tempfile.NamedTemporaryFile() as tmp_zip_file_obj:
+        #
+        #     zip_file = ZipFile(tmp_zip_file_obj.name, mode='w')
+        #
+        #     with tempfile.TemporaryDirectory() as tmpdirname:
+        #         files = []
+        #         for key in self.request.files.keys():
+        #             fp = tempfile.NamedTemporaryFile(dir=tmpdirname)
+        #             fp.write(self.request.files[key][0]['body'])
+        #             fp.seek(0)
+        #             files.append(fp)
+        #             zip_file.writestr('images/%s' % key, fp.read())
+        #
+        #         #md_file = StringIO()
+        #         #md_file.write(markdown_data)
+        #         zip_file.writestr('markdown.tex', markdown_data)
+        #         #md_file.close()
+        #
+        #
+        #     self.set_header('Content-Type', 'application/zip')
+        #     self.write(tmp_zip_file_obj.read())
+        #     self.finish()#print(tmp_zip_file_obj.closed)
+        # #self.write("Good")
 
 
 app = tornado.web.Application([
     (r'/render/', RenderHandler),
+    (r'/delete/(.*)', tornado.web.StaticFileHandler, {"path": "/Users/martin/Desktop"}),
     (r'/(.*)', tornado.web.StaticFileHandler, {"path": STATIC_DIR, "default_filename": "index.html"})
 ], debug=True)
 
 
 if __name__ == "__main__":
     app.listen(PORT)
+    print("Listening on port %s" % PORT)
     tornado.ioloop.IOLoop.instance().start()
--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -3,6 +3,86 @@ document.addEventListener('DOMContentLoaded', function(e){
 
 }, false);
 
+var file_selector = document.getElementById("file-selector");
+var form = document.getElementById("dataform");
+var files = [];
+var input_files = [];
+
+file_selector.onchange = function(e){
+
+    var re = /(.+?)(\.[^.]*$|$)/; //http://stackoverflow.com/a/624877/2628463
+
+    var file = this.files[0];
+    var same_name = files.filter(function(value, index){
+       return value.name == file.name;
+    });
+    var file_name = file.name;
+    if(same_name.length > 0){
+        split = re.exec(file.name);
+        if(split == null){
+            file_name = file.name + Math.random().toString(36).substring(7);
+        }else{
+            file_name = split[1] + Math.random().toString(36).substring(7) + split[2];
+        }
+    }
+    files.push({given_name:file_name, file:file});
+    this.value = "";
+
+    /*var input_file = document.createElement("input");
+    input_file.setAttribute("type", "file");
+    input_file.setAttribute("style", "display:none");
+    input_file.setAttribute("name", "file_"+input_files.length);
+    input_file.setAttribute("id", "id_"+input_files.length);
+    form.appendChild(input_file);
+    input_files.push(input_file);*/
+};
+
+form.onsubmit = function(e){
+    e.preventDefault();
+    var form_data = new FormData();
+    files.forEach(function(file){
+       form_data.append(file.given_name, file.file);
+    });
+    form_data.append("markdown-data", document.getElementById('text-input-area').value);
+    var xhr = new XMLHttpRequest();
+    /*xhr.open("POST", "/render/", true);
+    xhr.setRequestHeader("Accept","application/zip");
+    xhr.send(form_data);
+
+    xhr.onload = function(e){
+        if(this.status==200){
+            console.log(this);
+        }
+    }*/
+
+
+    xhr.open("GET", "/delete/demo.zip", true);
+    xhr.setRequestHeader("Accept", "application/zip");
+    xhr.responseType="blob";
+    xhr.onload = function(e) {
+      // response is unsigned 8 bit integer
+      var responseArray = new Uint8Array(this.response);
+        console.log(this);
+        saveAs(xhr.response, 'demo.zip')
+    };
+     xhr.send()
+
+    console.log(xhr)
+
+    /*var blob = new Blob([xhr.response], {type: "application/zip"});
+    saveAs(blob, 'file.zip');
+    return;
+    console.log(blob.size)
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = "Something.zip";
+    a.click();*/
+    //window.URL.revokeObjectURL(url);
+};
+
 var send = function(){
 	var text = document.getElementById('text-input-area').value;
 	console.log(text); return;

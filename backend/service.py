d2c6eba4041f6d07fec89b70c9262a16d469b6d8
Martin <martinarroyo@usal.es>
2016-03-18 20:52:55 +0100

    Markdown integration

--- backend/service.py
+++ backend/service.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import tornado.ioloop
 import tornado.web
+import tornado.gen
+
 import json
 import os, sys
 import tempfile
 from zipfile import ZipFile
+from mdx_latex import LaTeXExtension
+from markdown import Markdown
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 STATIC_DIR = os.path.join(CURRENT_DIR, 'static')
@@ -13,12 +17,20 @@ PORT = 9000
 
 class RenderHandler(tornado.web.RequestHandler):
 
+    def initialize(self):
+        self.md = Markdown()
+        mkdn2latex = LaTeXExtension(configs={}, maketitle=True)
+        mkdn2latex.extendMarkdown(self.md, Markdown.__dict__)
+
+    @tornado.web.asynchronous
     def post(self):
         markdown_data = self.get_body_argument('markdown-data', '')
 
+        processed_latex = self.md.convert(markdown_data)
+
         with tempfile.NamedTemporaryFile() as tmp_file_obj:
             zip_file = ZipFile(tmp_file_obj, mode='w')
-            zip_file.writestr('render/markdown.tex', markdown_data)
+            zip_file.writestr('render/markdown.tex', processed_latex)
             for key in self.request.files.keys():
                 zip_file.writestr('render/images/%s' % key, self.request.files[key][0]['body'])
             zip_file.close()
@@ -27,7 +39,6 @@ class RenderHandler(tornado.web.RequestHandler):
                 self.set_header('Content-Type', 'application/zip')
                 self.write(z.read())
                 self.finish()
-
 
 
 app = tornado.web.Application([
